eae2ffd6d00086f921e1e468fa9e261f611c8c00
LcReCeEs <rjriley08.rr@gmail.com>
2025-05-16 14:45:31 -0700

    Create new variables

--- globo_web_app/main.tf
+++ globo_web_app/main.tf
@@ -22,24 +22,20 @@ data "aws_ssm_parameter" "ami" {
 
 # NETWORKING #
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr_block
+  cidr_block           = var.aws_vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
-  
-  tags = local.common_tags
+
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
-  tags = local.common_tags
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block              = var.vpc_subnet1_cidr_block
+  cidr_block              = var.aws_subnet_cidr_block
   vpc_id                  = aws_vpc.vpc.id
   map_public_ip_on_launch = var.map_public_ip_on_launch
-
-  tags = local.common_tags
 }
 
 # ROUTING #
@@ -50,8 +46,6 @@ resource "aws_route_table" "rtb" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-
-  tags = local.common_tags
 }
 
 resource "aws_route_table_association" "rta-subnet1" {
@@ -67,27 +61,25 @@ resource "aws_security_group" "nginx-sg" {
 
   # HTTP access from anywhere
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = var.ingress_from_port
+    to_port     = var.ingress_to_port
+    protocol    = var.ingress_protocol
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # outbound internet access
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = var.egress_from_port
+    to_port     = var.egress_to_port
+    protocol    = var.egress_protocol
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = local.common_tags
 }
 
 # INSTANCES #
 resource "aws_instance" "nginx1" {
   ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
-  instance_type          = var.instance_type
+  instance_type          = var.aws_instance_type
   subnet_id              = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
 
@@ -99,6 +91,5 @@ sudo rm /usr/share/nginx/html/index.html
 echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
 EOF
 
-  tags = local.common_tags
-
 }
+
--- globo_web_app/variables.tf
+++ globo_web_app/variables.tf
@@ -1,55 +1,91 @@
 variable "aws_access_key" {
-    type = string
+    type        = string
     description = "AWS Access Key"
-    sensitive = true
+    sensitive   = true
 }
 
 variable "aws_secret_key" {
-    type = string
+    type        = string
     description = "AWS Secret Key"
-    sensitive = true
+    sensitive   = true
 }
 
 variable "aws_region" {
-    type = string
-    description = "AWS region to use for resources"
-    default = "us-west-1"
+    type        = string
+    description = "Which AWS region"
+    default     = "us-east-1"
 }
 
-variable "vpc_cidr_block" {
-    type = string
-    description = "Base CIDR Block for VPC"
-    default = "10.0.0.0/16"
+variable "aws_instance_type" {
+    type        = string
+    description = "AWS instance type"
+    default     = "t2.micro"
+}
+
+variable "aws_vpc_cidr_block" {
+    type        = string
+    description = "CIDR range"
+    default     = "10.0.0.0/16"
+}
+
+variable "aws_subnet_cidr_block" {
+    type        = string
+    description = "CIDR range"
+    default     = "10.0.0.0/24"
 }
 
 variable "enable_dns_hostnames" {
     type        = bool
-    description = "Enable DNS hostnames in VPC"
+    description = "Enable DNS hostname"
     default     = true
-}
-
-variable "vpc_subnet1_cidr_block" {
-    type        = string
-    description = "CIDR Block for Subnet 1 in VPC"
-    default     = "10.0.0.0/24"
 }
 
 variable "map_public_ip_on_launch" {
     type        = bool
-    description = "Map a public IP address for Subnet instances"
+    description = "Enable public ip"
     default     = true
 }
 
-variable "instance_type" {
-  type        = string
-  description = "Type for EC2 Instnace"
-  default     = "t2.micro"
+variable "ingress_from_port" {
+    type        = number
+    description = "Number"
+    default     = 80
+}
+
+variable "ingress_to_port" {
+    type        = number
+    description = "Number"
+    default     = 80
+}
+
+variable "ingress_protocol" {
+    type        = string
+    description = "Ingress Protocol"
+    default     = "tcp"
+}
+
+variable "egress_from_port" {
+    type        = number
+    description = "Number"
+    default     = 0
+}
+
+variable "egress_to_port" {
+    type        = number
+    description = "Number"
+    default     = 0
+}
+
+variable "egress_protocol" {
+    type        = string
+    description = "Ingress Protocol"
+    default     = "-1"
 }
 
 variable "company" {
   type        = string
   description = "Company name for resource tagging"
-  default = "Globomantics"
+  default     = "Globomantics"
 }
 
 variable "project" {
